da5c3b82ce9cdfa362992060ab15fed64ca29667
danchostic <fcse24-029@thuto.bac.ac.bw>
2025-11-25 23:13:27 +0000

    Initial commit for Tshepo Banking App

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import util.SupabaseClient;
-
-public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Fetching data from Supabase...");
-        String data = SupabaseClient.get("tshepobankingapp.customers");
-        System.out.println(data);
-    }
-}
