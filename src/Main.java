4635b7feea839c94e6bdb59df7434383e64f32c5
danchostic <fcse24-029@thuto.bac.ac.bw>
2025-11-25 23:38:00 +0000

    danchostic

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import util.SupabaseClient;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Fetching customers...");
+        String data = SupabaseClient.get("tshepobankingapp.customers");
+        System.out.println(data);
+    }
+}
